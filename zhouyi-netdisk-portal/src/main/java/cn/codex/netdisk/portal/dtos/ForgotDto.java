40801f2e86f1d1724dfb822c3144a0e400aeeae7
CcccX <422865236@qq.com>
2021-02-22 15:37:16 +0800

    忘记密码

---
+++ zhouyi-netdisk-portal/src/main/java/cn/codex/netdisk/portal/dtos/ForgotDto.java
@@ -0,0 +1,12 @@
+package cn.codex.netdisk.portal.dtos;
+
+import java.io.Serializable;
+
+/**
+ * 忘记密码
+ *
+ * @author CodeX
+ * @since 2021-02-22 15:12:11
+ */
+public class ForgotDto implements Serializable {
+}
