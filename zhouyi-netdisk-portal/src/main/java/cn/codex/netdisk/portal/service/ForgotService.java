d9eaa85616905419538b989a2353db7e3c72691c
CcccX <422865236@qq.com>
2021-03-01 09:45:08 +0800

    提取发送邮件验证码组件

--- zhouyi-netdisk-portal/src/main/java/cn/codex/netdisk/portal/service/ForgotService.java
+++ zhouyi-netdisk-portal/src/main/java/cn/codex/netdisk/portal/service/ForgotService.java
@@ -9,25 +9,14 @@ import cn.codex.netdisk.common.utils.RedisUtil;
 import cn.codex.netdisk.common.utils.RegexUtil;
 import cn.codex.netdisk.dao.UserMapper;
 import cn.codex.netdisk.model.entity.User;
-import cn.codex.netdisk.portal.config.PortalConfig;
+import cn.codex.netdisk.portal.component.CaptchaMail;
 import cn.codex.netdisk.portal.dtos.ForgotDto;
-import cn.hutool.core.util.IdUtil;
-import cn.hutool.core.util.RandomUtil;
 import cn.hutool.crypto.digest.BCrypt;
-import cn.hutool.log.Log;
-import cn.hutool.log.LogFactory;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.google.common.base.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.mail.MailProperties;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
-
-import javax.mail.internet.MimeMessage;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
 
 /**
  * 找回密码服务类
@@ -37,21 +26,14 @@ import java.util.concurrent.TimeUnit;
  */
 @Component
 public class ForgotService {
-    private static final Log log = LogFactory.get();
-
-    @Autowired
-    private JavaMailSender javaMailSender;
-
-    @Autowired
-    private MailProperties mailProperties;
-
     @Autowired
     private RedisUtil redisUtil;
 
     @Autowired
-    private PortalConfig portalConfig;
-    @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private CaptchaMail captchaMail;
 
     /**
      * 发送邮箱验证码
@@ -82,40 +64,7 @@ public class ForgotService {
             return ServerResponse.createByErrorMessage(ReturnMessage.EMAIL_ERROR);
         }
 
-        try {
-            MimeMessage message = javaMailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message);
-            // 发件人
-            helper.setFrom(mailProperties.getUsername());
-            // 收件人
-            helper.setTo(email);
-            helper.setSubject("【" + portalConfig.getNameZh() + "】找回密码");
-            // 发送日期
-            helper.setSentDate(new Date());
-            // 邮件内容
-            String code = RandomUtil.randomString(RandomUtil.BASE_CHAR_NUMBER + RandomUtil.BASE_CHAR.toUpperCase(), 4);
-            helper.setText("【" + portalConfig.getNameZh() + "】您用于找回密码的验证码为：<b>" + code + "</b>，有效期" + Const.FORGOT_CAPTCHA_EXPIRATION +
-                    "分钟，<b style='color:red;'>如非本人操作，请忽略该邮件内容，并尽快修改密码确保账号安全。</b>", true);
-
-            String uuid = IdUtil.simpleUUID();
-            try {
-                // 将验证码存入redis服务器中，并返回唯一标识
-                redisUtil.setObject(Const.FORGOT_KEY + uuid, code, Const.FORGOT_CAPTCHA_EXPIRATION, TimeUnit.MINUTES);
-            } catch (Exception ex) {
-                log.error("发送给 {} 的邮件发送失败=========> {}", email, ex.getMessage());
-                return ServerResponse.createByErrorMessage(ReturnMessage.CAPTCHA_SEND_ERROR);
-            }
-
-            // 发送邮件
-            javaMailSender.send(message);
-
-            log.info("发送给 {} 的邮件发送成功", email);
-            return ServerResponse.createBySuccess(ReturnMessage.CAPTCHA_SEND_SUCCESS, uuid);
-        } catch (Exception e) {
-            log.error("发送给 {} 的邮件发送失败=========> {}", email, e.getMessage());
-        }
-
-        return ServerResponse.createByErrorMessage(ReturnMessage.CAPTCHA_SEND_ERROR);
+        return captchaMail.sendCaptchaMail(email, "找回密码");
     }
 
     /**
