d02847def252a3485c3b62914f85b39eeaabc846
CcccX <422865236@qq.com>
2021-03-18 10:00:54 +0800

    tb_files添加file_type字段

--- zhouyi-netdisk-model/src/main/java/cn/codex/netdisk/model/entity/Files.java
+++ zhouyi-netdisk-model/src/main/java/cn/codex/netdisk/model/entity/Files.java
@@ -1,16 +1,13 @@
 package cn.codex.netdisk.model.entity;
 
-import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.annotation.IdType;
-import java.util.Date;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableLogic;
-import com.baomidou.mybatisplus.annotation.TableField;
-import java.io.Serializable;
+import com.baomidou.mybatisplus.annotation.*;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.util.Date;
 
 /**
  * <p>
@@ -49,6 +46,9 @@ public class Files implements Serializable {
 
     @ApiModelProperty(value = "文件大小")
     private Long size;
+
+    @ApiModelProperty(value = "文件类型")
+    private String fileType;
 
     @ApiModelProperty(value = "所属文件夹路径")
     private String dir;
