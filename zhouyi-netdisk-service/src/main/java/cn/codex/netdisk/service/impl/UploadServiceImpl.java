cfaec2cc64db78e4ae0a3d3cd66b7590fd07c40e
CcccX <422865236@qq.com>
2021-05-13 11:02:02 +0800

    优化文件上传逻辑

--- zhouyi-netdisk-service/src/main/java/cn/codex/netdisk/service/impl/UploadServiceImpl.java
+++ zhouyi-netdisk-service/src/main/java/cn/codex/netdisk/service/impl/UploadServiceImpl.java
@@ -4,7 +4,6 @@ import cn.codex.netdisk.common.constants.ReturnMessage;
 import cn.codex.netdisk.common.dtos.LoginUser;
 import cn.codex.netdisk.common.dtos.ServerResponse;
 import cn.codex.netdisk.common.enums.ResponseCode;
-import cn.codex.netdisk.common.utils.FileUtil;
 import cn.codex.netdisk.common.utils.SecurityUtil;
 import cn.codex.netdisk.dao.FilesMapper;
 import cn.codex.netdisk.dao.UserMapper;
@@ -32,13 +31,13 @@ import java.util.Date;
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class UploadServiceImpl implements IUploadService {
-    
+
     @Autowired
     private UserMapper userMapper;
-    
+
     @Autowired
     private FilesMapper filesMapper;
-    
+
     /**
      * 文件上传
      *
@@ -55,75 +54,42 @@ public class UploadServiceImpl implements IUploadService {
             return ServerResponse.createByErrorCodeMessage(ResponseCode.FILE_SIZE_OUT_OF_LIMIT.getCode(),
                     ResponseCode.FILE_SIZE_OUT_OF_LIMIT.getDesc());
         }
-        
-        // 文件扩展名 不带 "."
-        String extName = FileNameUtil.extName(file.getOriginalFilename());
-        
-        // 新文件名
-        String newFileName = dto.getIdentifier() + "." + extName;
-        
-        // 计算文件的存放目录
-        String path = storeDirectory + genericPath(newFileName, storeDirectory);
-        if (dto.getTotalChunks() == 1) {
-            // 判断文件大小是否超过剩余空间
-            if (isExcessStorageSpace(dto.getTotalSize())) {
-                return ServerResponse.createByErrorCodeMessage(ResponseCode.FILE_SIZE_OUT_OF_LIMIT.getCode(), ReturnMessage.INSUFFICIENT_SPACE);
-            }
-            
-            // 将文件保存到存放目录中
-            try {
-                File targetFile = new File(path, newFileName);
-                file.transferTo(targetFile);
-                
-                // 将文件信息保存到数据库
-                Files files = new Files();
-                files.setRealName(dto.getFilename());
-                files.setEncryptionName(newFileName);
-                files.setStoragePath(path + "/" + newFileName);
-                files.setExtension(extName);
-                files.setShortUrl("");
-                files.setSize(dto.getTotalSize());
-                files.setFileType(FileUtil.getFileType(new FileInputStream(targetFile), dto.getFilename()));
-                files.setIcon(FileUtil.getFileIcon(extName));
-                files.setDir(dto.getTargetPath());
-                files.setHidden(false);
-                files.setDeleted(false);
-                files.setCreator(SecurityUtil.getUsername());
-                filesMapper.insert(files);
-                
-                // 更新已用空间
-                updateUsedStorageSpace(dto.getTotalSize());
-                return ServerResponse.createBySuccessMessage(ReturnMessage.UPLOAD_SUCCESS);
-            } catch (Exception e) {
-                e.printStackTrace();
-                return ServerResponse.createByErrorMessage(ReturnMessage.UPLOAD_ERROR);
-            }
-        }
-        
-        // 分块数大于1
-        // 存储分块
-        for (int i = 0; i < dto.getTotalChunks(); i++) {
-            String tempFileName = dto.getChunkNumber() + "_" + newFileName + ".part";
-            File tempFile = new File(path, tempFileName);
-            if (!tempFile.exists()) {
-                try {
-                    file.transferTo(tempFile);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+
         OutputStream os = null;
         try {
-            // 合并分块
-            if (dto.getChunkNumber().equals(dto.getTotalChunks())) {
-                try {
-                    File targetFile = new File(path, newFileName);
+            // 文件扩展名 不带 "."
+            String extName = FileNameUtil.extName(file.getOriginalFilename());
+
+            // 新文件名
+            String newFileName = dto.getIdentifier() + "." + extName;
+
+            // 计算文件的存放目录
+            String path = storeDirectory + genericPath(newFileName, storeDirectory);
+
+            File targetFile = new File(path, newFileName);
+
+            if (dto.getTotalChunks() == 1) {
+                file.transferTo(targetFile);
+            } else {
+                String tempPath = storeDirectory + "/tempDir/" + dto.getIdentifier();
+                File tempDir = new File(tempPath);
+                if (!tempDir.exists()) {
+                    tempDir.mkdirs();
+                }
+
+                for (int i = 0; i < dto.getTotalChunks(); i++) {
+                    String tempFileName = dto.getChunkNumber() + "_" + dto.getIdentifier() + ".part";
+                    File tempFile = new File(tempPath, tempFileName);
+                    if (!tempFile.exists()){
+                        file.transferTo(tempFile);
+                    }
+                }
+                if (dto.getChunkNumber().equals(dto.getTotalChunks())) {
                     os = new BufferedOutputStream(new FileOutputStream(targetFile));
                     for (int i = 0; i < dto.getTotalChunks(); i++) {
-                        String tempFileName = (i + 1) + "_" + newFileName + ".part";
-                        File tempFile = new File(path, tempFileName);
-                        while (!tempFile.exists()) {
+                        String tempFileName = (i + 1) + "_" + dto.getIdentifier() + ".part";
+                        File tempFile = new File(tempPath, tempFileName);
+                        while (!tempFile.exists()){
                             Thread.sleep(100);
                         }
                         byte[] bytes = FileUtils.readFileToByteArray(tempFile);
@@ -132,12 +98,14 @@ public class UploadServiceImpl implements IUploadService {
                         tempFile.delete();
                     }
                     os.flush();
-                    return ServerResponse.createBySuccessMessage(ReturnMessage.UPLOAD_SUCCESS);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    return ServerResponse.createByErrorMessage(ReturnMessage.UPLOAD_ERROR);
+                    tempDir.delete();
                 }
             }
+
+            return ServerResponse.createBySuccessMessage(ReturnMessage.UPLOAD_SUCCESS);
+
+        } catch (Exception e) {
+            e.printStackTrace();
         } finally {
             if (os != null) {
                 try {
@@ -147,10 +115,10 @@ public class UploadServiceImpl implements IUploadService {
                 }
             }
         }
-        
+
         return ServerResponse.createByErrorMessage(ReturnMessage.UPLOAD_ERROR);
     }
-    
+
     /**
      * 秒传
      *
@@ -169,7 +137,7 @@ public class UploadServiceImpl implements IUploadService {
         // 更新已用空间
         updateUsedStorageSpace(files.getSize());
     }
-    
+
     /**
      * 判断文件大小是否超过剩余空间
      *
@@ -181,7 +149,7 @@ public class UploadServiceImpl implements IUploadService {
         Long maxStorageSpace = SecurityUtil.getLoginUser().getUser().getUserGroups().getMaxStorageSpace();
         return (usedStorageSpace + size) > maxStorageSpace;
     }
-    
+
     /**
      * 更新已用空间
      *
@@ -195,7 +163,7 @@ public class UploadServiceImpl implements IUploadService {
         updateUser.setUsedStorageSpace(user.getUsedStorageSpace() + size);
         userMapper.updateById(updateUser);
     }
-    
+
     /**
      * 计算文件的存放目录
      *
@@ -208,14 +176,14 @@ public class UploadServiceImpl implements IUploadService {
         int dir1 = hashCode & 0xf;
         // 二级目录
         int dir2 = (hashCode & 0xf0) >> 4;
-        
+
         String dir = "/" + dir1 + "/" + dir2;
-        
+
         File file = new File(storeDirectory, dir);
         if (!file.exists()) {
             file.mkdirs();
         }
-        
+
         return dir;
     }
 }
